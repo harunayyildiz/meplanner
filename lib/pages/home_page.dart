fbd1e412268590ddfabfb01a4cd53461f69f63cc
Harun Ayyıldız <harunayyildizz15@gmail.com>
2021-02-07 00:00:14 +0300

    code Update

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -39,57 +39,8 @@ class _HomePageState extends State<HomePage> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Container(
-                    margin: EdgeInsets.only(bottom: 12, top: 20),
-                    child: PopupMenuButton<String>(
-                      onSelected: (value) {
-                        if (value.compareTo('Tümü') == 0) {
-                          setState(() {
-                            filter = TodoFilter.ALL;
-                          });
-                        } else if (value.compareTo('Tamamlanmış') == 0) {
-                          setState(() {
-                            filter = TodoFilter.COMPLETED;
-                          });
-                        } else {
-                          setState(() {
-                            filter = TodoFilter.INCOMPLETED;
-                          });
-                        }
-                      },
-                      icon: Icon(
-                        Icons.toc,
-                        color: Colors.orange,
-                        size: 50,
-                      ),
-                      tooltip: 'Görevlerini Filtrele',
-                      padding: EdgeInsets.only(right: 10),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10)),
-                      itemBuilder: (BuildContext context) {
-                        return ["Tümü", "Tamamlanmış", "Tamamlanmamış"]
-                            .map((optional) {
-                          return PopupMenuItem(
-                            value: optional,
-                            child: Text(optional),
-                          );
-                        }).toList();
-                      },
-                    ),
-                  ),
-                  IconButton(
-                    onPressed: () {
-                      Box box = Hive.box('settings');
-                      box.put('isLighTheme',
-                          !box.get('isLighTheme', defaultValue: false));
-                      //Renginin tersini tekrar put ettik Tema değiştirildi.
-                    },
-                    icon: Icon(
-                      Icons.brightness_3,
-                      color: Colors.orange,
-                      size: 40,
-                    ),
-                  ),
+                  buildPopupFilter(),
+                  buildChangeThemeButton(),
                 ],
               ),
               Expanded(
@@ -114,82 +65,8 @@ class _HomePageState extends State<HomePage> {
                         }
 
                         return keys.length != 0
-                            ? ListView.builder(
-                                itemCount: keys.length,
-                                itemBuilder: (context, index) {
-                                  final key = keys[index];
-                                  //Key'e karşılık gelen değer(Model)
-                                  final TodoModel todo = todos.get(key);
-                                  return Dismissible(
-                                    key: UniqueKey(),
-                                    onDismissed: (DismissDirection drc) async {
-                                      if (drc == DismissDirection.endToStart) {
-                                        await todos.delete(key);
-                                        //Silinince Bildirim İptal
-                                        await LocalNotification()
-                                            .cancelNotification(key: key);
-                                      } else if (drc ==
-                                          DismissDirection.startToEnd) {
-                                        //Update
-                                        TodoModel mTodo = TodoModel(
-                                            title: todo.title,
-                                            description: todo.description,
-                                            isDone: true);
-                                        await todos.put(key, mTodo);
-                                        //Tamamlanınca Bildirim iptal
-                                        await LocalNotification()
-                                            .cancelNotification(key: key);
-                                      }
-                                    },
-                                    // resizeDuration: Duration(milliseconds: 500),
-                                    secondaryBackground: Align(
-                                      alignment: Alignment.centerRight,
-                                      child: Icon(
-                                        Icons.delete,
-                                        size: 50,
-                                      ),
-                                    ),
-                                    background: Align(
-                                      alignment: Alignment.centerLeft,
-                                      child: Icon(
-                                        Icons.check_circle_outline,
-                                        size: 50,
-                                      ),
-                                    ),
-                                    direction: DismissDirection.horizontal,
-                                    child: TodoWidget(
-                                      todoKey: key,
-                                      title: todo.title,
-                                      desc: todo.description,
-                                      isDone: todo.isDone,
-                                    ),
-                                  );
-                                },
-                              )
-                            : Container(
-                                margin: EdgeInsets.all(40),
-                                child: Column(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceEvenly,
-                                  children: [
-                                    Image(
-                                      image: AssetImage(
-                                          'assets/images/noItem2.png'),
-                                    ),
-                                    Padding(
-                                      padding: const EdgeInsets.only(top: 20),
-                                      child: Text(
-                                        textlisten(),
-                                        textAlign: TextAlign.center,
-                                        style: TextStyle(
-                                            fontSize: 23,
-                                            fontWeight: FontWeight.bold,
-                                            fontStyle: FontStyle.italic),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              );
+                            ? buildListView(keys, todos)
+                            : buildNoItem();
                       }),
                 ),
               ),
@@ -215,6 +92,138 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+  Widget buildListView(List<int> keys, Box<TodoModel> todos) {
+    return ListView.builder(
+      itemCount: keys.length,
+      itemBuilder: (context, index) {
+        final key = keys[index];
+        //Key'e karşılık gelen değer(Model)
+        final TodoModel todo = todos.get(key);
+        return Dismissible(
+          key: UniqueKey(),
+          onDismissed: (DismissDirection drc) async {
+            if (drc == DismissDirection.endToStart) {
+              await todos.delete(key);
+              //Silinince Bildirim İptal
+              await LocalNotification().cancelNotification(key: key);
+            } else if (drc == DismissDirection.startToEnd) {
+              //Update
+              TodoModel mTodo = TodoModel(
+                  title: todo.title,
+                  description: todo.description,
+                  isDone: true);
+              await todos.put(key, mTodo);
+              //Tamamlanınca Bildirim iptal
+              await LocalNotification().cancelNotification(key: key);
+            }
+          },
+          // resizeDuration: Duration(milliseconds: 500),
+          secondaryBackground: Align(
+            alignment: Alignment.centerRight,
+            child: Icon(
+              Icons.delete,
+              size: 50,
+            ),
+          ),
+          background: Align(
+            alignment: Alignment.centerLeft,
+            child: Icon(
+              Icons.check_circle_outline,
+              size: 50,
+            ),
+          ),
+          direction: DismissDirection.horizontal,
+          //Listview Elemanı Custom Container
+          child: TodoWidget(
+            todoKey: key,
+            title: todo.title,
+            desc: todo.description,
+            isDone: todo.isDone,
+          ),
+        );
+      },
+    );
+  }
+
+  Widget buildPopupFilter() {
+    return Container(
+      margin: EdgeInsets.only(bottom: 12, top: 20),
+      child: PopupMenuButton<String>(
+        onSelected: (value) {
+          if (value.compareTo('Tümü') == 0) {
+            setState(() {
+              filter = TodoFilter.ALL;
+            });
+          } else if (value.compareTo('Tamamlanmış') == 0) {
+            setState(() {
+              filter = TodoFilter.COMPLETED;
+            });
+          } else {
+            setState(() {
+              filter = TodoFilter.INCOMPLETED;
+            });
+          }
+        },
+        icon: Icon(
+          Icons.toc,
+          color: Colors.orange,
+          size: 50,
+        ),
+        tooltip: 'Görevlerini Filtrele',
+        padding: EdgeInsets.only(right: 10),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        itemBuilder: (BuildContext context) {
+          return ["Tümü", "Tamamlanmış", "Tamamlanmamış"].map((optional) {
+            return PopupMenuItem(
+              value: optional,
+              child: Text(optional),
+            );
+          }).toList();
+        },
+      ),
+    );
+  }
+
+  IconButton buildChangeThemeButton() {
+    return IconButton(
+      onPressed: () {
+        Box box = Hive.box('settings');
+        box.put('isLighTheme', !box.get('isLighTheme', defaultValue: false));
+        //Renginin tersini tekrar put ettik Tema değiştirildi.
+      },
+      icon: Icon(
+        Icons.brightness_3,
+        color: Colors.orange,
+        size: 40,
+      ),
+    );
+  }
+
+  Container buildNoItem() {
+    return Container(
+      margin: EdgeInsets.all(40),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Image(
+            image: AssetImage('assets/images/noItem2.png'),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 20),
+            child: Text(
+              textlisten(),
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 23,
+                  fontWeight: FontWeight.bold,
+                  fontStyle: FontStyle.italic),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   String textlisten() {
     String text = 'Hadi Görev Ekle ve Başla!';
     if (todoBox.length > 0) {
@@ -226,4 +235,18 @@ class _HomePageState extends State<HomePage> {
     }
     return text;
   }
+
+  // //Update after List suffle
+  // List shuffle(List items) {
+  //   var random = new Random();
+  //   for (var i = items.length - 1; i > 0; i--) {
+  //     var n = random.nextInt(i + 1);
+
+  //     var temp = items[i];
+  //     items[i] = items[n];
+  //     items[n] = temp;
+  //   }
+
+  //   return items;
+  // }
 }
